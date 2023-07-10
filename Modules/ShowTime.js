896d3dffaa51796f19962cf8ad10df86ae8f56ca
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-07-10 16:13:35 +0500

    Remove Comments

--- Modules/ShowTime.js
+++ Modules/ShowTime.js
@@ -5,15 +5,6 @@ function ShowTime(time){
     const now = time.now();
     let formattedDateTime = now.toFormat("MMMM d'th' yyyy, h:mm:ss a")
     formattedDateTime = formattedDateTime.replace('AM', 'am').replace('PM', 'pm');
-    // const formattedDateTime = now.toLocaleString({
-    //   month: 'long',
-    //   day: 'numeric',
-    //   year: 'numeric',
-    //   hour: 'numeric',
-    //   minute: 'numeric',
-    //   second: 'numeric',
-    //   hour12: true,
-    // });
     Time.textContent = formattedDateTime
   }, 1)
 }
